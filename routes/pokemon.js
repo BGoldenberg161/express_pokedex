6d7cd4ba3c8255d8fcbc1e4cf36abf5f8b9c7f54
Branden Goldenberg <brandengoldenberg@MacBook-Pro.local>
2020-08-09 14:40:23 -0600

    [css]add style and comments

--- routes/pokemon.js
+++ routes/pokemon.js
@@ -6,7 +6,9 @@ const db = require('../models')
 
 // GET /pokemon - return a page with favorited Pokemon
 router.get('/', async (req, res) => {
+  // check for errors
   try {
+    //if no errors, get data, render favorites
     const faveData = await db.pokemon.findAll()
     res.render('favorites', {pokemon: faveData})
   } catch (err) { 
@@ -17,8 +19,25 @@ router.get('/', async (req, res) => {
 
 // POST /pokemon - receive the name of a pokemon and add it to the database
 router.post('/', async (req, res) => {
+  // check for errors
   try {
+    // if no errirs, create new(if doesn't already exist)
     await db.pokemon.findOrCreate({
+      where: {
+        name: req.body.name
+      }
+    })
+    // load favorites page
+    res.redirect('/pokemon')
+  } catch (err){ 
+    console.log('Error:', err) // render error
+  }
+})
+
+// Delete pokemon from favorites
+router.delete('/', async (req, res) => {
+  try {
+    await db.pokemon.destroy({
       where: {
         name: req.body.name
       }
@@ -28,10 +47,11 @@ router.post('/', async (req, res) => {
     console.log('Error:', err) // render error
   }
 })
+
 //get details(show)
 router.get('/:name', async (req, res) => {
   try {
-    if (req.params && req.params.name) {
+    if (req.params.name) {
       const pokeUrl = `https://pokeapi.co/api/v2/pokemon/${req.params.name.toLowerCase()}`
       const result = await axios.get(pokeUrl)
       let pokemonData = result.data
@@ -41,18 +61,7 @@ router.get('/:name', async (req, res) => {
     console.log('Error:', err) // render error
   }
 })
-// Delete /pokemon - receive the name of a pokemon and add it to the database
-router.delete('/', async (req, res) => {
-  try {
-    await db.pokemon.destroy({
-      where: {
-        name: req.body.name,
-      }
-    })
-    res.redirect('/pokemon')
-  } catch (err){ 
-    console.log('Error:', err) // render error
-  }
-})
+
+
 
 module.exports = router;
